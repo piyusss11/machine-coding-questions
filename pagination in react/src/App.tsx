25e11b14d083ada884f85b613239cacee11949a8
piyusss11 <piyusss11@gmail.com>
2025-02-04 22:18:06 +0530

    completed

--- pagination in react/src/App.tsx
+++ pagination in react/src/App.tsx
@@ -6,6 +6,10 @@ interface Product {
 }
 function App() {
   const [products, setProducts] = useState<Product[]>([]);
+  const [currentProducts, setCurrentProducts] = useState<Product[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  
   const TOTALPRODUCTS = products.length;
   const PAGE_SIZE = 10;
   const TOTAL_PAGES = Math.ceil(TOTALPRODUCTS / PAGE_SIZE);
@@ -14,6 +18,7 @@ function App() {
     const response = await fetch("https://dummyjson.com/products?limit=0");
     const data = await response.json();
     setProducts(data.products);
+    setCurrentProducts(data.products.slice(0, PAGE_SIZE));
   };
   useEffect(() => {
     fetchData();
@@ -26,12 +31,24 @@ function App() {
         <div className="text-center text-2xl flex gap-2">
           <div className="flex mx-auto my-4 justify-center items-center">
             {[...Array(TOTAL_PAGES).keys()].map((page) => (
-              <span key={page} className="border-1 p-2 mx-1 text-center">{page + 1}</span>
+              <span
+                onClick={() => {
+                  setCurrentProducts(
+                    products.slice(page * PAGE_SIZE, (page + 1) * PAGE_SIZE)
+                  );
+                  setCurrentPage(page + 1);
+                }}
+                key={page}
+                className={`cursor-pointer border-1 p-2 mx-1 text-center ${page+1 === currentPage ? "bg-blue-500 text-white" : ""}`}
+              >
+                
+                {page + 1}
+              </span>
             ))}
           </div>
         </div>
         <div className="flex justify-center flex-wrap gap-4">
-          {products.map((product) => (
+          {currentProducts.map((product) => (
             <div
               className="border-1 w-60 h-60 flex-col flex justify-center items-center p-4"
               key={product.id}
